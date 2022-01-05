b0692d68f25dd147a9bd9d59dc22edd183668643
Ionel Condor <ionel_condor@yahoo.com>
2022-01-05 20:22:17 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("demo git");
-        System.out.println("demo din nou");
+        System.out.println("demo din nou");;
+	    //demo
     }
 }
