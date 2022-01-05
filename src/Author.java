fd026861f153267dee1571049327e16a85f36a68
icondor <icondor@sdl.com>
2022-01-05 20:49:18 +0200

    first version

---
+++ src/Author.java
@@ -0,0 +1,21 @@
+public class Author {
+
+    private String name;
+    private boolean isPreferred;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPreferred() {
+        return isPreferred;
+    }
+
+    public void setPreferred(boolean preferred) {
+        isPreferred = preferred;
+    }
+}
